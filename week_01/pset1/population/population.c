06a537fdcae11ed0a71965880738eccefb1b97a9
JuliaMathias <juliamss@gmail.com>
2021-06-03 21:45:22 -0300

    add population program

---
+++ week_01/pset1/population/population.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start_size;
+    do
+    {
+      start_size = get_int("How many Lhamas live here? ");;
+    }
+    while (start_size < 9);
+
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
