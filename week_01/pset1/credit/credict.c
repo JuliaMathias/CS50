a531c207f4e6cb7a6258f29ac366b19138e4b790
JuliaMathias <juliamss@gmail.com>
2021-06-15 14:15:51 -0300

    fix typos

--- week_01/pset1/credit/credict.c
+++ week_01/pset1/credit/credict.c
@@ -6,8 +6,10 @@ int find_length (long long digits);
 bool checksum(long long card_number);
 void reveal_brand(long long card_number);
 
+
 int main(void)
-{   long long card
+{   long long card;
+
     do
     {
     card = get_long_long("Number: ");
@@ -27,27 +29,28 @@ int main(void)
 bool validate(long long card)
 {
     int length = find_length(card);
-    return (length == 13 || length == 15 || length == 16) && checksum(card)
+    return (length == 13 || length == 15 || length == 16) && checksum(card);
 }
 
 int find_length (long long digits)
 {
     int length;
-    for (length = 0; digits != 0; digits /= 10; length++)
-    return length
+    for (length = 0; digits != 0; digits /= 10, length++);
+    return length;
 }
 
 bool checksum(long long card_number)
 {
     int sum = 0;
-    for (int i = 0; card_number != 10; i++, card_number /= 10)
+    int i = 0;
+    for (i = 0; card_number != 0; i++, card_number /= 10)
     {
         if (i % 2 == 0)
             sum += card_number % 10;
         else
             {
                 int digit = 2 * (card_number % 10);
-                sum += digit / 10 + digits % 10;
+                sum += digit / 10 + digit % 10;
             }
     }
     return (sum % 10) == 0;
@@ -57,18 +60,18 @@ void reveal_brand(long long card_number)
 {
     if ((card_number >= 34e13 && card_number < 35e13) || (card_number >= 37e13 && card_number < 38e13))
     {
-        printf("AMEX\n")
+        printf("AMEX\n");
     }
     else if (card_number >= 51e14 && card_number < 56e14)
     {
-        printf("MASTERCARD\n")
+        printf("MASTERCARD\n");
     }
     else if ((card_number >= 4e12 && card_number < 5e12) || (card_number >= 4e15 && card_number < 5e15))
     {
-        printf("VISA\n")
+        printf("VISA\n");
     }
     else
     {
-        printf("INVALID\n")
+        printf("INVALID\n");
     }
 }
