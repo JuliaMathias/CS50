a13bf887e6b1709570b21bfe6be3f707358410ed
JuliaMathias <juliamss@gmail.com>
2021-06-29 14:20:05 -0300

    print hi using index

--- week2/lecture2/hi.c
+++ week2/lecture2/hi.c
@@ -1,9 +1,10 @@
-// Prints string
+// Prints string's ASCII codes
 
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string s = "HI!";
-    printf("%s\n", s);
+    printf("%i %i %i\n", s[0], s[1], s[2]);
 }
