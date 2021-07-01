79b8c13e538aa955cfc015813d2ae2e4046af16a
JuliaMathias <juliamss@gmail.com>
2021-07-01 14:25:51 -0300

    refactor: print each character in a different line

--- week2/lecture2/argv.c
+++ week2/lecture2/argv.c
@@ -1,12 +1,17 @@
-// Prints command-line arguments
+// Prints characters in an array of strings
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     for (int i = 0; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
+        for (int j = 0, n = strlen(argv[i]); j < n; j++)
+        {
+            printf("%c\n", argv[i][j]);
+        }
+        printf("\n");
     }
 }
