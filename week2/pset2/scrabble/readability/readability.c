4d25d2467563f0f41b3db1faa332974515419938
JuliaMathias <juliamss@gmail.com>
2021-07-09 14:03:54 -0300

    feat: pseudocode

---
+++ week2/pset2/scrabble/readability/readability.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    // Get input text
+    string text = get_string("Text: ");
+
+
+    // TODO: Print the readability level
+        //TODO: split the text into words
+        //TODO: calculate the number of words in the text
+        //TODO: calculate the average of letters per 100 words in the text
+        //TODO: calculate the number of sentences in the text
+        //TODO: calculate the average of sentences per 100 words in the text
+
+        index = 0.0588 * letter_average - 0.296 * sentence_average - 15.8
+}
+
